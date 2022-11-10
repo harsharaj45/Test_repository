87a5df0f97fffe77137a8a206434126b41d0206f
harsharaj45 <harsharaj45@gmail.com>
2022-11-10 11:15:25 +0530

    5th commit

---
+++ java program 11/src/sample/HelloWorld.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class HelloWorld {
+	public void display()
+	{
+		System.out.println("hello world");
+	}
+
+	public static void main(String[] args) {
+		HelloWorld obj=new HelloWorld();
+		obj.display();
+
+	}
+
+}
